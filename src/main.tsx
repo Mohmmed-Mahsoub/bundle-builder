71911f43eefc2cf8338940541482f58d1eb9b949
Mohmmed-Mahsoub <mohamedmahsoub181@gmail.com>
2026-07-13 22:30:47 +0200

    chore: setup redux and redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { Provider } from "react-redux"
+import { persistor, store } from "./store/store.ts"
+import { PersistGate } from "redux-persist/integration/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 )
